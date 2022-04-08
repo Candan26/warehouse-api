5c4ae929ceaa88e03b4d8ae0330c3d952b03fc90
cagri <cagricandan91@gmail.com>
2022-04-09 01:32:41 +0300

    **** Test case added **** -> Fix on InterceptorConfig -> Test case added for auth

--- src/main/java/com/readingisgood/warehouseapi/config/InterceptorConfig.java
+++ src/main/java/com/readingisgood/warehouseapi/config/InterceptorConfig.java
@@ -15,6 +15,6 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(initializeAuthInterceptor()).addPathPatterns("/**").excludePathPatterns("/login");
+        registry.addInterceptor(initializeAuthInterceptor()).addPathPatterns("/**").excludePathPatterns("/auth");
     }
 }
